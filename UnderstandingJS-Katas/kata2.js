28f2aafebdcab09a5b25d93dc844705c205e6f7d
Arielle Moon <amoon@icstars.org>
2026-02-20 20:48:13 -0600

    refactored with clear variable and commented!

--- UnderstandingJS-Katas/kata2.js
+++ UnderstandingJS-Katas/kata2.js
@@ -4,14 +4,14 @@
 // Commit 2: "added fizzbuzz logic"
 // Commit 3: "refactored with clear variable and comments"
 
-for (let i = 1; i <= 20; i++) { /*let the starting variable ('i') start at '1', and the loop run, adding 1 every loop ('i++'), until the variable ('i') is less than or equal to '20' */
-  if (i % 3 === 0 && i % 5 === 0) { /*setting the logic for variables accounted for to be divisible by 3 AND 5! (evident by the remainder operator equalling zero after being divided by 3 and ('&&') 5)*/
+for (let clapping = 1; clapping <= 20; clapping++) { /*let the starting variable ('i') start at '1', and the loop run, adding 1 every loop ('i++'), until the variable ('i') is less than or equal to '20' */
+  if (clapping % 3 === 0 && clapping % 5 === 0) { /*setting the logic for variables accounted for to be divisible by 3 AND 5! (evident by the remainder operator equalling zero after being divided by 3 and ('&&') 5)*/
     console.log("FizzBuzz"); /*FizzBuzz (15:) being the logic for %3 && %5 (divisible by 3 and 5) */
-  } else if (i % 3 === 0) {
+  } else if (clapping % 3 === 0) {
     console.log("Fizz"); /*Fizz (3,6,9,etc.) being the logic for %3*/
-  } else if (i % 5 === 0) {
+  } else if (clapping % 5 === 0) {
     console.log("Buzz"); /*Buzz (5,10,15,etc.) being the logic for %5*/
   } else {
-    console.log(i); /*Running the code to check for outcome:]*/
+    console.log(clapping); /*Running the code to check for outcome:]*/
   }
-}
+}/*replaced 'i' with clapping to mimic your example from the lesson, and equated the coding to a round in choir music, where every 'i' equals a clap until a divisible # of 3 and 5 signals someone to say Fizz or Buzz:]*/
