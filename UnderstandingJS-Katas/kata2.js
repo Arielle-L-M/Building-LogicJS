25b392f4dfc144e61a2deb7d3a7fe4518b5817bc
Arielle Moon <amoon@icstars.org>
2026-02-20 20:35:33 -0600

    scaffold for loop comment

--- UnderstandingJS-Katas/kata2.js
+++ UnderstandingJS-Katas/kata2.js
@@ -4,7 +4,7 @@
 // Commit 2: "added fizzbuzz logic"
 // Commit 3: "refactored with clear variable and comments"
 
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) { /*let the starting variable ('i') start at '1', and the loop run, adding 1 every loop ('i++'), until the variable ('i') is less than or equal to '20' */
   if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
   } else if (i % 3 === 0) {
