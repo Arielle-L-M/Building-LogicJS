bc721cf72cea33dd5f38ac5a68c5a698d84cc459
Arielle Moon <amoon@icstars.org>
2026-02-20 19:45:37 -0600

    scaffold loop structure comment

--- UnderstandingJS-Katas/kata1.js
+++ UnderstandingJS-Katas/kata1.js
@@ -5,7 +5,7 @@
 // Commit 3: "refactored variable names for clarity"
 
 
-for (let i = 1; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) { /* 'let i=1' sets the starting variable as 1, 'i <=' sets the loop to run so long as the variable remains less than or equal to 20 while the loop runs, and 'i++' means add 1 to the initail variable every loop that continues until they hit that '<=20' condition*/
   if (i % 2 === 0) {
     console.log(i);
   }
